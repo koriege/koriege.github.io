1d5033cd7a044eeb5a35a64af368d2edd1d5760b
Konstantin Riege <koriege@users.noreply.github.com>
2026-08-27 09:36:19 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,13 +19,13 @@ $(function() {
     $("nav ul li:first-child a").parent().addClass("active");
   });
 
-  $("nav ul li").on("click", "a", function(event) {
-    var position = $($(this).attr("href")).offset().top - 190;
-    $("html, body").animate({scrollTop: position}, 400);
-    $("nav ul li a").parent().removeClass("active");
-    $(this).parent().addClass("active");
-    event.preventDefault();
-  });
+  // $("nav ul li").on("click", "a", function(event) {
+  //   var position = $($(this).attr("href")).offset().top - 190;
+  //   $("html, body").animate({scrollTop: position}, 400);
+  //   $("nav ul li a").parent().removeClass("active");
+  //   $(this).parent().addClass("active");
+  //   event.preventDefault();
+  // });
 
   // sectionHeight();
 
