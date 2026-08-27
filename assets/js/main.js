1ed0f150d3b27f2fdada4a061b80ddf462203f94
Konstantin Riege <koriege@users.noreply.github.com>
2026-08-27 12:03:59 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,7 @@ var sectionHeight = function() {
 
   if ($section.outerHeight(true) < total) {
     var margin = $section.outerHeight(true) - $section.height();
-    $section.height(total - margin - 51);
+    $section.height(total - margin - 171);
   } else {
     $section.css('height','auto');
   }
